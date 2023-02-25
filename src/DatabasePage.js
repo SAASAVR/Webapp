5176fefea481e7663b39c47006eb2c23a491ad12
mrcromero <mrcromero33@gmail.com>
2023-02-24 19:42:47 -0700

    add new hardware page files

---
+++ src/DatabasePage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class DatabasePage extends React.Component {
+    render() {
+        return (
+            <h1>Database</h1>
+        );
+    }
+}
+
+export default DatabasePage
---
+++ src/HardwarePage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class HardwarePage extends React.Component {
+    render() {
+        return (
+            <h1>Hardware</h1>
+        );
+    }
+}
+
+export default HardwarePage
