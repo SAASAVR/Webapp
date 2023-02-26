3ae0d18c7e12c3146915718afb83fe2eba118dcc
mrcromero <mrcromero33@gmail.com>
2023-02-26 00:19:33 -0700

    add status panel

--- src/HardwarePage.js
+++ src/HardwarePage.js
@@ -6,19 +6,24 @@ class HardwarePage extends React.Component {
     render() {
         return (
             <div className="Hardware-Page">
-                <div className="Hardware-Panel Settings-Panel">
-                    <h3 className="Panel-Title">Settings</h3>
-                    <div className="Settings-Option">
-                        <p>Interval</p>
+                <div className="Left-Side">
+                    <div className="Hardware-Panel Status-Panel">
+                        <h3 className="Panel-Title">Status</h3>
                     </div>
-                    <div className="Settings-Option">
-                        <p>Sensor-activated</p>
-                    </div>
-                    <div className="Settings-Option">
-                        <p>Threshold</p>
+                    <div className="Hardware-Panel Settings-Panel">
+                        <h3 className="Panel-Title">Settings</h3>
+                        <div className="Settings-Option">
+                            <p>Interval</p>
+                        </div>
+                        <div className="Settings-Option">
+                            <p>Sensor-activated</p>
+                        </div>
+                        <div className="Settings-Option">
+                            <p>Threshold</p>
+                        </div>
                     </div>
                 </div>
-                <div className="Hardware-Panel Stream-Panel">
+                <div className="Hardware-Panel Right-Side Stream-Panel">
                     <h3 className="Panel-Title">Stream</h3>
                 </div>
             </div>
