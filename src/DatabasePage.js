9390779b45f3542411eceb48afdddc15ebb27a0a
Darius Fang <56571687+DarFang@users.noreply.github.com>
2023-03-01 22:42:33 -0700

    init db page placeholders

--- src/DatabasePage.js
+++ src/DatabasePage.js
@@ -1,11 +1,25 @@
 import React from "react"
 
-import './App.css'
+import ReactSelect from "react-select";
+import ReactSwitch from "react-switch";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import './DatabasePage.css'
+import './App.css'
+import * as states from './States.js';
 class DatabasePage extends React.Component {
     render() {
         return (
-            <h1>Database</h1>
+            <div className="Database-Page">
+                <div className="Left-Side">
+                    <div className="Database-Panel Left-Side List-Panel">
+                        <h3 className="Panel-Title">Status</h3>
+                        </div>
+                        </div>
+                <div className="Database-Panel Right-Side Audio-Panel">
+                    <h3 className="Panel-Title">Stream</h3>
+                </div>
+            </div>
         );
     }
 }
